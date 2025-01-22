d9951cb523a7fc8e176496939b8b2caf1ce3c528
nikothomas <nikojb@gmail.com>
2025-01-21 23:01:23 -0800

    docker

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+// vite-env.d.ts
+declare module '*?worker' {
+    const workerConstructor: {
+        new (): Worker
+    }
+    export default workerConstructor
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
-
-// https://vite.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
@@ -10,6 +8,10 @@ export default defineConfig({
       '@assets': path.resolve(__dirname, './src/assets'),
       '@components': path.resolve(__dirname, './src/components'),
     },
+  },
+  preview: {
+    port: 8080,
+    strictPort: true,
   },
   server: {
     port: 8080,
